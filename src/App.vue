994a6ab122853adc221a0b6a9f2763bc500c3259
Soyoung Shim <ellie.shim.s@gmail.com>
2019-04-16 13:56:28 +0900

    오름차순, 내림차순 구현

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
       <div class="row">
         <h3 class="col-xs-12 text-center">코멘토 과제전형-심소영</h3>
       </div>
+      <div class="filter row">
+        <div class="col-xs-4">
+          <button type="button">필터</button>
+        </div>
+        <div class="col-xs-6 col-xs-offset-2 text-right">
+          <button type="button" @click="toggleAsc()">오름차순</button>
+          <button type="button" @click="toggleDesc()">내림차순</button>
+        </div>
+      </div>
       <ul class="contents row">
         <li class="contents__item list-group col-xs-12" v-for="i in contentsList" v-bind:key="i.no">
           <p class="contents__header">
@@ -39,6 +48,23 @@ export default {
   computed: {},
 
   methods: {
+    toggleAsc() {
+      if (this.ord === "desc") {
+        this.ord = "asc";
+        this.contentsList = [];
+        this.page = 1;
+        this.getList(this.ord);
+      }
+    },
+    toggleDesc() {
+      if (this.ord === "asc") {
+        this.ord = "desc";
+        this.contentsList = [];
+        this.page = 1;
+
+        this.getList(this.ord);
+      }
+    },
     findCategory(no) {
       // 불러온 contentsList 데이터의 category_no 값을 이용해서 category의 name을 찾는 함수.
       // category_no을 인수로 받아서 category에서 no값이 인수로 들어온 category_no값과 일치하는
@@ -55,11 +81,11 @@ export default {
       // 불러서 category에 넣는다.
       this.category = this.category.concat(data.list);
     },
-    async getList() {
+    async getList(ord) {
       // 글 목록을 불러오는 함수. 무한스크롤을 위해 page 값, 정렬을 위해 ord값을 사용한다.
-      let api = `http://comento.cafe24.com/request.php?page=${this.page}&ord=${
-        this.ord
-      }`;
+      let api = `http://comento.cafe24.com/request.php?page=${
+        this.page
+      }&ord=${ord}`;
       console.log(api);
       const { data } = await axios.get(api);
       this.contentsList = this.contentsList.concat(data.list);
@@ -72,13 +98,13 @@ export default {
       ) {
         if (this.page < 100) {
           this.page += 1;
-          this.getList();
+          this.getList(this.ord);
         }
       }
     }
   },
   async mounted() {
-    this.getList();
+    this.getList(this.ord);
     await this.getCategory();
     await this.findCategory();
     window.addEventListener("scroll", e => this.onScroll(), false);
@@ -93,6 +119,13 @@ export default {
 
 <style lang="less">
 @import "./index.less";
+.filter {
+  button {
+    background-color: rebeccapurple;
+    color: white;
+    cursor: pointer;
+  }
+}
 .contents {
   &__item {
     border: 1px solid @primary;
