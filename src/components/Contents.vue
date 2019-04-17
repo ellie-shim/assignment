f05d3419d1dec22fecc5d82e2ef374bdbf537dfa
Soyoung Shim <ellie.shim.s@gmail.com>
2019-04-17 20:54:22 +0900

    주석 추가

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -14,31 +14,29 @@
         </div>
       </div>
       <ul class="contents row">
-        <li
-          class="contents__item list-group col-xs-12"
-          v-for="(i, index) in contentsList"
-          :key="index"
-        >
-          <p class="contents__header">
-            <span class="col-xs-6">{{findCategory(i.category_no).name}}</span>
-            <span class="col-xs-6 text-right">{{i.no}}</span>
-          </p>
-          <p class="contents__info col-xs-12">
-            <span>{{i.email}}</span>
-            <span>{{i.updated_at}}</span>
-          </p>
-          <router-link :to="i.no">
-            <h3 class="list-group-item-headeing col-xs-12">{{i.title}}</h3>
-            <p class="list-group-item-text col-xs-12">{{i.contents}}</p>
-          </router-link>
-        </li>
+        <!--  글 목록 -->
+        <template v-for="(i, index) in contentsList">
+          <li  class="contents__item list-group col-xs-12" v-if="(index+1)%4!== 0" :key="index">
+            <p class="contents__header">
+              <span class="col-xs-6">{{findCategory(i.category_no).name}}</span>
+              <span class="col-xs-6 text-right">{{i.no}}</span>
+            </p>
+            <p class="contents__info col-xs-12">
+              <span>{{i.email}}</span>
+              <span>{{i.updated_at}}</span>
+            </p>
+            <router-link :to="i.no">
+              <h3 class="list-group-item-headeing col-xs-12">{{i.title}}</h3>
+              <p class="list-group-item-text col-xs-12">{{i.contents}}</p>
+            </router-link>
+          </li>
+        </template>
       </ul>
     </div>
     <modal
       ref="filterModal"
       :category="category"
       :categoryFilter="categoryFilter"
-      @handleClickCategory="handleClickCategory"
       @saveCategory="saveCategory"
     ></modal>
   </div>
@@ -52,28 +50,28 @@ export default {
   name: "Contents",
   data() {
     return {
+      // 글 목록이 담긴 배열
       contentsList: [],
+      // 파라미터로 넘길 페이지
       page: 1,
+      // 파라미터로 넘길 정렬 값
       ord: "asc",
+      // 카테고리 목록이 담긴 배열
       category: [],
-      categoryFilter: [1, 2, 3]
+      // (카테고리 필터링 기능을 위한 ) 파라미터로 넘길 카테고리 값
+      categoryFilter: [1, 2, 3],
     };
   },
-  computed: {},
-
   methods: {
+    // 모달창에서 필터링 한 카테고리를 저장하고 데이터를 다시 불러오는 함수
     async saveCategory(selected) {
-      console.log(selected);
       this.categoryFilter = selected.slice();
       this.$refs.filterModal.close();
       this.contentsList = [];
       this.page = 1;
       this.getList(this.ord, this.categoryFilter);
     },
-    handleClickCategory(categories) {
-      console.log(categories);
-      // this.categoryFilter = categories.slice();
-    },
+    // 오름차순으로 만드는 함수
     toggleAsc() {
       if (this.ord === "desc") {
         this.ord = "asc";
@@ -82,6 +80,7 @@ export default {
         this.getList(this.ord, this.categoryFilter);
       }
     },
+    // 내림차순으로 변경하는 함수
     toggleDesc() {
       if (this.ord === "asc") {
         this.ord = "desc";
@@ -90,34 +89,41 @@ export default {
 
         this.getList(this.ord, this.categoryFilter);
       }
-    },
+    },    
     findCategory(no) {
       // 불러온 contentsList 데이터의 category_no 값을 이용해서 category의 name을 찾는 함수.
       // category_no을 인수로 받아서 category에서 no값이 인수로 들어온 category_no값과 일치하는
       // 인덱스를 찾고, category의 이 인덱스 값의 name 을 출력 => 했었는데 에러나서 .name은 템플릿에서 설정
-
       const index = this.category.findIndex(i => i.no === no);
       return this.category[index];
     },
+    // category 목록을 불러오는 함수.
     async getCategory() {
-      // category 목록을 불러오는 함수.
-      const { data } = await axios.get(
-        "http://comento.cafe24.com/category.php"
-      );
-      // 불러서 category에 넣는다.
-      this.category = this.category.concat(data.list);
+      try {
+        const { data } = await axios.get(
+          "http://comento.cafe24.com/category.php"
+        );
+        // 불러서 category에 넣는다.
+        this.category = this.category.concat(data.list);
+      } catch (e) {
+        throw new Error(e);
+      }
     },
+    // 글 목록을 불러오는 함수. 무한스크롤을 위해 page 값, 정렬을 위해 ord값을 사용한다.
     async getList(ord, category) {
-      // 글 목록을 불러오는 함수. 무한스크롤을 위해 page 값, 정렬을 위해 ord값을 사용한다.
-      let api = `http://comento.cafe24.com/request.php?page=${
-        this.page
-      }&ord=${ord}&category=${category}`;
-      console.log(api);
-      const { data } = await axios.get(api);
-      this.contentsList = this.contentsList.concat(data.list);
+      try {
+        let api = `http://comento.cafe24.com/request.php?page=${
+          this.page
+        }&ord=${ord}&category=${category}`;
+        console.log(api);
+        const { data } = await axios.get(api);
+        this.contentsList = this.contentsList.concat(data.list);
+      } catch (e) {
+        throw new Error(e);
+      }
     },
+    // 무한스크롤 함수. 스크롤바가 바닥에 닿으면 page+1 하고 다시 통신한다.
     onScroll() {
-      // 무한스크롤 함수. 스크롤바가 바닥에 닿으면 page+1 하고 다시 통신한다.
       if (
         window.innerHeight + window.pageYOffset >=
         document.body.offsetHeight
@@ -125,18 +131,19 @@ export default {
         if (this.page < 100) {
           this.page += 1;
           this.getList(this.ord, this.categoryFilter);
+          this.getAds();
+
         }
       }
     }
   },
-  async mounted() {
+  async created() {
+    this.getAds(); 
     this.getList(this.ord, this.categoryFilter);
     await this.getCategory();
     await this.findCategory();
     window.addEventListener("scroll", () => this.onScroll(), false);
   },
-  created() {},
-  destroyed() {},
   components: {
     Modal
   }
@@ -146,10 +153,13 @@ export default {
 <style lang="less">
 @import "../index.less";
 .filter {
+  margin: 10px 0;
   button {
     background-color: rebeccapurple;
     color: white;
     cursor: pointer;
+    margin-right: 10px;
+    padding: 8px;
   }
 }
 .contents {
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,23 +2,26 @@
   <!-- transition : transition으로 감싸진 엘리먼트가 on/off될때 일어남 -->
   <transition name="modal">
     <div class="modal" v-show="active">
-      <ul class="modal__con">
+      <div class="modal__con">
+        <h3>필터</h3>
         <button type="button" class="close" @click="close()">
           &times;
           <span class="readable-hidden">모달창 닫기 버튼</span>
         </button>
-        <li v-for="i in category" v-bind:key="i.no">
-          <input
-            type="checkbox"
-            :name="i.name"
-            :id="i.name"
-            :value="i.no"
-            v-model="selectedCategories"
-          >
-          <label :for="i.name">{{i.name}}</label>
-        </li>
-        <button type="button" @click="saveCategory()">저장</button>
-      </ul>
+        <ul>
+          <li v-for="i in category" v-bind:key="i.no">
+            <input
+              type="checkbox"
+              :name="i.name"
+              :id="i.name"
+              :value="i.no"
+              v-model="selectedCategories"
+            >
+            <label :for="i.name">{{i.name}}</label>
+          </li>
+        </ul>
+        <button type="button" @click="saveCategory()" class="modal__save">저장</button>
+      </div>
     </div>
   </transition>
 </template>
@@ -38,25 +41,23 @@ export default {
   },
   data() {
     return {
+      // 기존에 선택되어 있는 카테고리 
       selectedCategories: [1, 2, 3],
+      // 모달 on/off를 위한 값
       active: false
     };
   },
   methods: {
+    // 모달 on
     open() {
       this.active = true;
     },
+    // 모달 off
     close() {
       this.active = false;
     },
 
-    handleClickCategory() {
-      this.$emit("handleClickCategory");
-    },
-    handleCategoryFilter() {
-      console.log(this.categoryFilter);
-      this.$emit("update:categoryFilter", this.selectedCategories);
-    },
+    // 부모 컴포넌트로부터 받은 함수 : 선택한 카테고리를 저장해서 부모에게 넘겨준다.
     saveCategory() {
       this.$emit("saveCategory", this.selectedCategories);
     }
@@ -69,16 +70,39 @@ export default {
 .modal {
   width: 100%;
   height: 100vh;
-  background-color: gray;
+  background-color: rgba(0, 0, 0, 0.7);
   display: block;
   &__con {
     padding: 20px;
     background-color: #fff;
     width: 50%;
+    max-width: 350px;
+    position: relative;
+    h3 {
+      margin-top: 0;
+    }
+    ul {
+      .clearfix();
+      margin-top: 10px;
+      li {
+        float: left;
+        margin-right: 15px;
+        input {
+          margin-right: 3px;
+        }
+      }
+    }
     .center-block();
     .close {
       padding: 5px;
+      position: absolute;
+      top: 1rem;
+      right: 2rem;
     }
+  }
+  &__save {
+    float: right;
+    margin-top: 10px;
   }
 }
 </style>
