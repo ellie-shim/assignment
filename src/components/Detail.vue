e7a3473a86d6f8a5eb7d5d60fe5e057eb0bb352d
Soyoung Shim <ellie.shim.s@gmail.com>
2019-04-17 20:54:15 +0900

    글 상세보기 페이지 구현

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -6,37 +6,60 @@
       <p>{{this.content.updated_at}}</p>
       <p>{{this.content.contents}}</p>
     </div>
-    <div class="detail__repl">
-      <!-- 리플이 있으면 확인 후 뿌려주기 -->
+    <div class="detail__repl" v-if="this.repl.length>0">
+      <h4>댓글 목록</h4>
+      <ul>
+        <li v-for="r in repl" :key="r.no">
+          <p>{{r.user_no}}번 회원님</p>
+          <p>{{r.updated_at}}</p>
+          <p>{{r.contents}}</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 <script>
 import axios from "axios";
-import { log } from "util";
 
 export default {
   name: "Detail",
   data() {
     return {
-      content: {},
-      repl: {}
+        // 글 내용을 담은 객체
+        content: {},
+        // 글에 대한 리플을 담은 베열
+        repl: []
     };
   },
   methods: {
+    // 글 내용을 불러오는 함수
     async getDetail(contentsId) {
+        // contentsId 를 이용해서 데이터를 호출한다.
       let api = `http://comento.cafe24.com/detail.php?req_no=${contentsId}`;
       const {
         data: { detail }
       } = await axios.get(api);
-      console.log(detail);
       this.content = detail.article;
       this.repl = detail.replies;
     }
   },
   created() {
+      // 주소표시줄에 있는 값을 가져와서 파라미터로 넘겨준다.
     this.getDetail(this.$route.params.contentsId);
   }
 };
 </script>
+<style lang="less">
+.detail {
+  &__repl {
+    margin-top: 40px;
+    li {
+      background-color: beige;
+      padding: 20px;
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
+
 
